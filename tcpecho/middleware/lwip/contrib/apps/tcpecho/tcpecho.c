590cd206277733cc95cbd6716c4ccd11c69886e4
michaeldaang <78927304+michaeldaang@users.noreply.github.com>
2022-05-11 15:55:55 +0200

    updatetest

--- tcpecho/middleware/lwip/contrib/apps/tcpecho/tcpecho.c
+++ tcpecho/middleware/lwip/contrib/apps/tcpecho/tcpecho.c
@@ -82,7 +82,7 @@ tcpecho_thread(void *arg)
         do {
 
 
-        	vTaskDelay(100000);
+        	vTaskDelay(50);
         	 long stop = xTaskGetTickCount() ; //test get tick counter
         	 long time=  stop - start;
         	 //buf->p->payload = &i;
